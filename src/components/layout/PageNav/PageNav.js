afb23190a3aa27dfc52008a6d6f279951447b2a8
Nali <nalikante@gmail.com>
2021-11-01 12:43:19 +0100

    Add waiter nad kitchen

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -8,6 +8,8 @@ const PageNav = () => (
     <nav>
       <Button className={styles.link} component ={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Button>
       <Button className={styles.link} component ={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+      <Button className={styles.link} component ={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+      <Button className={styles.link} component ={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
     </nav>
   </div>
 );
